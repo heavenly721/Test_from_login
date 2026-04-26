e3811781d9222b6cf724eb412dedb31e794368ce
Mishail <Marvin721@yandex.ru>
2026-04-26 21:50:50 +0300

    correct Test

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,13 +1,19 @@
 package pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
 
+    private SelenideElement dateOfBirthInput = $("#dateOfBirthInput");
+    private SelenideElement monthSelect = $(".react-datepicker__month-select");
+    private SelenideElement yearSelect = $(".react-datepicker__year-select");
+
     public void setDate(String day, String month, String year) {
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(year);
+        dateOfBirthInput.click();
+        monthSelect.selectOption(month);
+        yearSelect.selectOption(year);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
     }
 }
